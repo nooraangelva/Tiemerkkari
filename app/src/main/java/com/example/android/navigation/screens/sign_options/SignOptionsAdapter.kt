55d5bcf2985c05652feb2ca21087bf9f72335460
nooraangelva <nooraangelva@gmail.com>
2021-10-22 10:58:09 +0300

    Recycle view lesson9.9

--- app/src/main/java/com/example/android/navigation/screens/sign_options/SignOptionsAdapter.kt
+++ app/src/main/java/com/example/android/navigation/screens/sign_options/SignOptionsAdapter.kt
@@ -1,13 +1,20 @@
 package com.example.android.navigation.screens.sign_options
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.navigation.R
 import com.example.android.navigation.database.Signs
 
-abstract class SignOptionsAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+
+
+class SignOptionsAdapter: RecyclerView.Adapter<SignOptionsAdapter.ViewHolder>(){
 
     var data =  listOf<Signs>()
         set(value){
@@ -16,8 +23,14 @@ abstract class SignOptionsAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
         }
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
+        val res = holder.itemView.context.resources
+        holder.signName.text = item.signName
+
+        holder.image.setImageResource(item.sourcePicture)
+
+
         /*to reset or set properties to existing views
         if(item.signName == "red"){
             holder.textView.setColor(Color.RED)
@@ -28,10 +41,15 @@ abstract class SignOptionsAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
          */
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
-                .inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+                .inflate(R.layout.list_item_signs, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val signName: TextView = itemView.findViewById(R.id.signNameListText)
+        val image: ImageView = itemView.findViewById(R.id.imageViewList)
     }
 }
